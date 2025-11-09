1b8eaf40448e4fdb2f42fbedc2b9aa70e1588f1d
virtual <iidai.informatica.unsxx@gmail.com>
2025-11-08 22:03:25 -0400

    Delete Pagina directory
    Signed-off-by: virtual <iidai.informatica.unsxx@gmail.com>

--- Pagina/script.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("contactForm").addEventListener("submit", function(e) {
-    e.preventDefault();
-    alert("Gracias por tu mensaje. Nos pondremos en contacto pronto.");
-    this.reset();
-});
